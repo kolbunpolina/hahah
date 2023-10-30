53bd2c461415acb9e372a77015f048d97beb59de
kolbunpolina <129773893+kolbunpolina@users.noreply.github.com>
2023-10-30 19:47:41 +0300

    Update game.py

--- game.py
+++ game.py
@@ -6,6 +6,32 @@ size = 18 # размер шрифта
 W,H = 600,600
 win = pg.display.set_mode((W,H)) # создание игрового окна
 name = ''
+class Player(pg.sprite.Sprite):
+    def __init__(self,x,y):
+        pg.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+        self.speed_x = 0
+        self.apeed_y = 1
+        self.image = pg.image.load('zm.png')
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.x
+        self.rect.bottom = self.y
+    def update(selfself):
+        for i in range(4):
+            win.blit(self.image,(self.rect.x-100*i,self.rect.y))
+            self.ract.x += self.speed_x
+            self.y
+all_sprites = pg.sprite.Group()
+player = Player(300,300)
+all_sprites.add(player)
+def draw_text(surf, text, x,y, size=size, colir=(255,255,255)):
+    def update(selfself):
+        self.ract.x += self.speed_x
+        self.ract.y += self.speed_y
+        key = pg.key.get_prassed()
+        if key[pg.K_LEFT]:
+            self.speed_x = -1
 def draw_text(surf, text, x,y,size=size, color=(255,255,255)):
     font = pg.font.Font(font_name, size) # определение шрифта
     text_surface = font.render(text,True,color)
@@ -40,6 +66,8 @@ while True:
         if i.type == pg.QUIT:
             exit()
     win.fill((255, 255, 255))
+    all_sprites.update()
+    all_sprites.draw(win)
     for y in range(0,H,25):
         for x in range(0,W,25):
             pg.draw.line(win,(0,255,0), (0,y), (W, y))
